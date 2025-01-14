ec01decba13c4bf5cab17af642af957cc5eb566b
Anton Nomed <antonnomed@gmail.com>
2025-01-14 13:51:20 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+  name='{Quizz_Game-NOMED_Anton-CDOF5}',
+  version='1.0',
+  author='{NOMED Anton}',
+  license='MIT',
+  long_description=open('README.md').read(),
+)
